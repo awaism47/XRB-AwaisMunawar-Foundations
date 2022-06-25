4e0b1e9f54a2bfad58f0c989e7e854d8a7f04f2e
awaism47 <awaismunawar789@gmail.com>
2022-06-25 12:34:21 +0100

    mission complete assignment

--- Assets/Scripts/GoalTrigger.cs
+++ Assets/Scripts/GoalTrigger.cs
@@ -2,17 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEditor;
+using UnityEngine.UI;
 
 public class GoalTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject goal;
+    [SerializeField] private GameObject canvas;
+    [SerializeField] private GameObject effect;
 
     private void OnTriggerEnter(Collider other)
     {
+        
+        
         if (other.GetComponent<GoalInteractor>())
         {
-            Debug.Log("You have reached your Goal!");
+
+            StartCoroutine(Wait());
+            
         }
+    }
+    private IEnumerator Wait()
+    {
+        effect.SetActive(true);
+        yield return new WaitForSeconds(3);
+        canvas.transform.GetChild(1).gameObject.SetActive(true);
+        canvas.transform.GetChild(2).gameObject.SetActive(true);
+        Debug.Log("You have reached your Goal!");
+
     }
 
 }
