a34cf8718275a8559f64b48de205062eb460c621
awaism47 <awaismunawar789@gmail.com>
2022-06-25 12:56:55 +0100

    added falling cube

--- Assets/Scripts/GoalTrigger.cs
+++ Assets/Scripts/GoalTrigger.cs
@@ -2,15 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using UnityEditor;
 using UnityEngine.UI;
 
 public class GoalTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject goal;
-    [SerializeField] private GameObject canvas;
-    [SerializeField] private GameObject effect;
+    [SerializeField] private GameObject cube;
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -18,19 +17,9 @@ public class GoalTrigger : MonoBehaviour
         
         if (other.GetComponent<GoalInteractor>())
         {
+            cube.AddComponent<Rigidbody>();
 
-            StartCoroutine(Wait());
-            
+
         }
     }
-    private IEnumerator Wait()
-    {
-        effect.SetActive(true);
-        yield return new WaitForSeconds(3);
-        canvas.transform.GetChild(1).gameObject.SetActive(true);
-        canvas.transform.GetChild(2).gameObject.SetActive(true);
-        Debug.Log("You have reached your Goal!");
-
-    }
-
 }
