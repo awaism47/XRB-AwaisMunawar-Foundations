a872254445533ec2fc05b752265815eafdbd8cd5
awaism47 <awaismunawar789@gmail.com>
2022-07-02 14:25:09 +0100

    completed enemy patrol routes

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,50 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] private NavMeshAgent agent;
-    [SerializeField] private float threshold=0.5f;
-    [SerializeField] private Transform point1;
-
-    [SerializeField] private Transform point2;
-    private Transform currentPoint;
-
-    private bool moving = false;
+    [SerializeField] private NavMeshAgent _agent;
+    [SerializeField] private float _threshold=0.5f;
+    [SerializeField] private PatrolRoute _patrolRoute;
     
+    private Transform _currentPoint;
+    private bool _moving = false;
+    //Need to create integer to allow us to loop through each point on each frame
+    private int _routeIndex = 0;
+    private bool _movingForward = true;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _currentPoint = _patrolRoute.route[_routeIndex];
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (!moving)
+        if (!_moving)
         {
-            if (currentPoint == point1)
+            NextPatrolPoint();
+            _agent.SetDestination(_currentPoint.position);
+            _moving = true;
+
+        }
+
+        if (_moving && Vector3.Distance(transform.position,_currentPoint.position)<_threshold)
+        {
+            _moving = false;
+        }
+    }
+
+    private void NextPatrolPoint()
+    {
+        //If Loop then always moving forward but if not then moving backwards after reaching 3rd point
+        
+        if (_movingForward)
+        {
+            _routeIndex++;
+            
+        }
+        else
+        {
+            _routeIndex--;
+        }
+        
+        
+        if (_routeIndex == _patrolRoute.route.Count)
+        {
+            if (_patrolRoute.patrolType==PatrolRoute.PatrolType.Loop)
             {
-                currentPoint=point2;
+                _routeIndex = 0;
             }
             else
             {
-                currentPoint = point1;
+                _movingForward = false;
+                _routeIndex -= 2;
             }
-            agent.SetDestination(currentPoint.position);
-            moving = true;
-            
             
         }
 
-        if (moving && Vector3.Distance(transform.position,currentPoint.position)<threshold)
+        if (_routeIndex == 0)
         {
-            
-            
-            moving = false;
+            _movingForward = true;
         }
+
+        _currentPoint = _patrolRoute.route[_routeIndex];
     }
 }
---
+++ Assets/Scripts/PatrolRoute.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEditor;
+using UnityEngine;
+
+public class PatrolRoute : MonoBehaviour
+{
+    //Good practice to hardcode enum with integers to not confuse unity
+    public enum PatrolType
+    {
+        Loop =0,
+        PingPong=1
+    }
+    //Serialising enum to allow for drop down
+    [SerializeField] public PatrolType patrolType;
+    [SerializeField] public List<Transform> route;
+    [SerializeField] private Color _patrolRouteColor;
+
+    
+    [Button("Add Patrol Point")]
+    private void AddPatrolPoint()
+    {
+        GameObject thisPoint = new GameObject();
+        thisPoint.transform.position = transform.position;
+        thisPoint.transform.parent = transform;
+        thisPoint.name = "Point" + (route.Count + 1);
+        route.Add(thisPoint.transform);
+        
+        #if UNITY_EDITOR
+        Undo.RegisterCreatedObjectUndo(thisPoint,"Petrol point undo");
+        #endif
+    }
+
+    [Button("Reverse Patrol Route")]
+    private void ReversePatrolRoute()
+    {
+        route.Reverse();
+    }
+
+    private void OnDrawGizmos()
+    {
+        #if UNITY_EDITOR
+        Handles.Label(transform.position,gameObject.name);
+        #endif
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = _patrolRouteColor;
+        
+        for (int i = 0; i < route.Count - 1; i++)
+        {
+            Gizmos.DrawLine(route[i].position,route[i+1].position);
+        }
+
+        if (patrolType == PatrolType.Loop)
+        {
+            Gizmos.DrawLine(route[route.Count-1].position,route[0].position);
+        }
+       
+
+    }
+}
